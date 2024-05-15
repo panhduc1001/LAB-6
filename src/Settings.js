5059f4df4eecf6901b17123dd0f5055a6c9a3ba9
Anh Duc Phan <jacksonp.se1@gmail.com>
2024-05-15 17:11:34 +1000

    V2: Rebuild UI

---
+++ src/Settings.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Photo from "./avatar.png";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+
+const Settings = () => {
+	return () => {
+		<div>
+			<h1>HELLO GUYS</h1>
+		</div>;
+	};
+};
+
+export default Settings;
